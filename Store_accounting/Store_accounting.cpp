41429279d06cdd9e48b632be21aa68b53b52f711
m0misL <macsim.banchenko@yandex.ru>
2026-02-26 15:32:48 +0500

    adding structs, funcs, main WIP

--- Store_accounting/Store_accounting.cpp
+++ Store_accounting/Store_accounting.cpp
@@ -1,20 +1,110 @@
-﻿// Store_accounting.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <Windows.h>
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+//preprocessor
+#define MAX_DISCOUNT 30
+#define SERVICE_COMMISION 7
+
+#define CHECK_PRICE(price) static_assert(price > 0)
+
+struct Item {
+    string name;
+    double price;
+    int quantity;
+//constructor    
+    Item(string n, double p, int q) :name(n), price(p), quantity(q) {
+        if (price < 0) {
+            cout << "Item price must be positive.";
+        }
+        else if (price = 0) {
+            cout << "Free item";
+        }
+    }
+    //total price including quantity
+    double TotalPriceGet() {
+        return price * quantity;
+    }
+};
+
+struct Order {
+    int id;
+    string customer;
+    vector<Item> items;
+    
+    Order(int i, string c) :id(i), customer(c) {
+        if (id < 0) {
+            cout << "Order does not exist.";
+        }
+    }
+//add item in order
+    void ItemAdd(const Item& item) {
+        items.push_back(item);
+    }
+    //order summary
+    double TotalAmountGet() {
+        double total = 0;
+        for (auto& item : items) { //loop for calculation sum of prices
+            total += item.TotalPriceGet();
+
+        }
+        return total;
+    }
+    //order summary with Discount
+    double TotalAmountGetD() {
+        double total = TotalAmountGet();
+        return total * (100 - MAX_DISCOUNT) / 100;
+    }
+};
+
+
+struct Store {
+    vector<Order> orders;
+    //add order
+    void OrderAdd(const Order& order) {
+        orders.push_back(order);
+    }
+    //revenue total
+    double TotalRevenueGet() {
+        double total = 0;
+        for (auto& order : orders) {
+            total += order.TotalAmountGet();
+        }
+        return total;
+    }
+    //revenue total with discount
+    double TotalRevenueGetD() {
+        double total = 0;
+        for (auto& order : orders) {
+            total += order.TotalAmountGetD();
+        }
+        return total;
+    }
+    //service commission
+    double ServiceCommisionGet() {
+        return TotalRevenueGet() * SERVICE_COMMISION / 100;
+    }
+
+
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Store store;
+
+    Order order1(1, "Ivan Ivanov");
+    order1.ItemAdd(Item("Laptop", 29999, 1));
+    order1.ItemAdd(Item("Thermopaste", 305, 2));
+
+    Order order2(2, "Dmitriy Vasilev");
+    order2.ItemAdd(Item("MotherBoard", 9999, 1));
+    order2.ItemAdd(Item("CPU", 10500, 1));
+    order2.ItemAdd(Item("GPU", 16650, 1));
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
